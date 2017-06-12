7daf21a99891caf3779dcd61af04da726b05c26c
elpinal <elpinal@users.noreply.github.com>
2017-06-12 22:06:03 +0900

    Implement the skelton

--- src/aewo.ml
+++ src/aewo.ml
@@ -8,4 +8,11 @@ let usage () =
          uninstall    Uninstall a specified version of Emacs.
          " |> print_endline;;
 
-let () = usage ();;
+let run = function
+        | "install" -> "installing..." |> print_endline
+        | "uninstall" -> "uninstalling..." |> print_endline
+        | name -> "no such command: " ^ name |> prerr_endline; exit 1
+
+let () =
+        if Array.length Sys.argv == 1 then usage ()
+        else run Sys.argv.(1)
