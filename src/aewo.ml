38cc43ef2cb5250609d32daf78b6a54674eef29c
elpinal <elpinal@users.noreply.github.com>
2017-06-17 18:05:04 +0900

    Update syntax for plainness

--- src/aewo.ml
+++ src/aewo.ml
@@ -20,10 +20,10 @@ let exec_dir dir cmd args =
   let pid = Unix.fork () in
   if pid == 0 then (
     Unix.chdir dir;
-    Unix.execvp cmd (Array.append [|cmd|] args))
-  else
+    Unix.execvp cmd (Array.append [|cmd|] args)
+  ) else
     match snd (Unix.waitpid [] pid) with
-      WEXITED 0 -> ()
+    | WEXITED 0 -> ()
     | WEXITED code -> failwith (Printf.sprintf "%s exited with code: %d" cmd code)
     | WSIGNALED signal -> failwith (Printf.sprintf "%s was killed with signal: %d" cmd signal)
     | WSTOPPED signal -> failwith (Printf.sprintf "%s was stopped with signal: %d" cmd signal)
@@ -40,42 +40,41 @@ let ensure_dir dirname =
 let init uri =
   if not (Sys.file_exists root) then (
     Unix.mkdir root 0o777;
-    exec "git" [| "clone"; "--mirror"; uri; (Filename.concat root "repo") |])
+    exec "git" [| "clone"; "--mirror"; uri; (Filename.concat root "repo") |]
+  )
 
 let checkout version =
   ensure_dir (Filename.concat root "emacs");
   let dir =
-    (Filename.concat (Filename.concat root "emacs") version)
+    Filename.concat (Filename.concat root "emacs") version
   in
-  exec "git" [| "clone"; (Filename.concat root "repo"); dir |];
+  exec "git" [| "clone"; Filename.concat root "repo"; dir |];
   exec_dir dir "git" [| "reset"; "--hard"; version |]
 
 let build version =
   let dir =
-    (Filename.concat (Filename.concat root "emacs") version)
+    Filename.concat (Filename.concat root "emacs") version
   in
   exec_dir dir "./autogen.sh" [||];
   exec_dir dir "./configure" [| "--without-ns"; "--without-x" |];
   exec_dir dir "make" [| "-k"; "-j4" |]
 
 let is_symlink x =
-  let open Unix
-  in
-  let stats = lstat x
-  in
+  let open Unix in
+  let stats = lstat x in
   stats.st_kind == S_LNK
 
 let link version =
   let src =
-    (Filename.concat (Filename.concat (Filename.concat root "emacs") version) "src/emacs")
+    Filename.concat (Filename.concat (Filename.concat root "emacs") version) "src/emacs"
   in
-  if not (Sys.file_exists src) then
-    (Printf.sprintf "linking %s: executable does not exist" version |> prerr_endline; exit 1);
+  if not (Sys.file_exists src) then (
+    Printf.sprintf "linking %s: executable does not exist" version |> prerr_endline; exit 1
+  );
   ensure_dir (Filename.concat root "bin");
-  let dest = (Filename.concat root "bin/emacs")
-  in
+  let dest = Filename.concat root "bin/emacs" in
   (* If dest is symlink, Sys.file_exists does not return true. So, || is used, not &&. *)
-  if (Sys.file_exists dest) || (is_symlink dest) then
+  if Sys.file_exists dest || is_symlink dest then
     Sys.remove dest;
   Unix.symlink src dest
 
@@ -85,7 +84,8 @@ let install = function
     init default_uri; (* update; *)
     checkout version;
     build version;
-    link version)
+    link version
+  )
 
 (* TODO: implement this *)
 let uninstall version = ()
